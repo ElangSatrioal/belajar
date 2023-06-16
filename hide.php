bfb5e2201459c95102d2ef6e981a0c786a1e9326
ElangSatrioal <elangsatrioalalyyu@gmail.com>
2023-06-16 14:27:18 +0700

    up

---
+++ hide.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Syariah</title>
+    <link rel="stylesheet" type="text/css" href = "css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <header>
+        <nav class= "navbar">
+            <div class = "container">
+            <a href="" class = "logo">Hotel Syariah</a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Contant</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
